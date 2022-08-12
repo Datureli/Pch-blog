1730f23e8f7a837be586c15ca76416cb743cd9b2
Datureli <soleris1@gmail.com>
2022-08-12 21:37:33 +0200

    fix displaying content only on blog page

--- template-parts/posts.php
+++ template-parts/posts.php
@@ -3,35 +3,34 @@ $wpb_all_query = new WP_Query(
 	[
 		'post_type'      => 'post',
 		'post_status'    => 'publish',
-		'posts_per_page' => 6,
+		'posts_per_page' => 4,
+        'has_archive'   => false,
 	] 
 ); ?>
- 
-<?php if ( $wpb_all_query->have_posts() ) : ?>
+
+<?php if ( have_posts() && is_home()  ) : ?>
  
 <ul style="list-style: none;">
  
 	<?php 
-	while ( $wpb_all_query->have_posts() ) :
-		$wpb_all_query->the_post(); 
+	while ( have_posts() ) :
+		the_post(); 
 		?>
 		<li style="display: flex;">
-            
-        </style> >
-		
-			<div class="thumbnail-img">
+			
+		</style> 
+
 			   <?php if ( has_post_thumbnail() ) : ?>
-                <a href="<?php the_permalink(); ?>">
+				<a href="<?php the_permalink(); ?>">
 					<?php the_post_thumbnail( 'medium' ); ?>	
-               </a>		
+			   </a>		
 			   <?php endif; ?>
+			<div>
+			<a href="<?php the_permalink(); ?>"><?php the_title( '<h2>', '</h2>'); ?> </a>
+			  <?php the_excerpt(); ?> 
+			  <?php echo get_the_date( get_option( 'date_format' ) ); ?>
 			</div>
-            <div>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a>
-			<?php the_content(); ?> 
-            <?php echo get_the_date( get_option( 'date_format' ) ); ?>
-            </div>
-         
+		 
 		</li>
 
 	<?php endwhile; ?>
@@ -39,10 +38,12 @@ $wpb_all_query = new WP_Query(
 </ul>
 
 	<?php wp_reset_postdata(); ?>
-    <div style="justify-content: center; margin: auto; display: flex;">
-  <h2> <?php the_posts_pagination(); ?></h2> 
-    </div>
+	<div style="justify-content: center; margin: auto; display: flex;">
+		<h2><?php the_posts_pagination(); ?></h2> 
+	</div>
    
 <?php else : ?>
-	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php if(is_home())  : ?>
+	<p><?php _e( 'Sorry, no posts here' ); ?></p>
+    <?php endif; ?>
 <?php endif; ?>
