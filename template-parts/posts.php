48c218f8df90adff7989e05954309d582fd72167
Datureli <soleris1@gmail.com>
2022-08-12 16:48:23 +0200

    remove unused code

--- template-parts/posts.php
+++ template-parts/posts.php
@@ -4,8 +4,6 @@ $wpb_all_query = new WP_Query(
 		'post_type'      => 'post',
 		'post_status'    => 'publish',
 		'posts_per_page' => -1,
-		'fields'         => 'ids',
-		$data = get_posts( $args ),
 	] 
 ); ?>
  
@@ -17,22 +15,28 @@ $wpb_all_query = new WP_Query(
 	while ( $wpb_all_query->have_posts() ) :
 		$wpb_all_query->the_post(); 
 		?>
-		<li >
-			<a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a>
-			<?php the_content(); ?> 
+		<li style="display: flex;">
+            
+        </style> >
+		
 			<div class="thumbnail-img">
 			   <?php if ( has_post_thumbnail() ) : ?>
-					<?php the_post_thumbnail( 'medium' ); ?>			
+                <a href="<?php the_permalink(); ?>">
+					<?php the_post_thumbnail( 'medium' ); ?>	
+               </a>		
 			   <?php endif; ?>
-			   <?php echo get_the_date( get_option( 'date_format' ) ); ?>
-			   
 			</div>
+            <div>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a>
+			<?php the_content(); ?> 
+            <?php echo get_the_date( get_option( 'date_format' ) ); ?>
+            </div>
+         
 		</li>
 
 	<?php endwhile; ?>
  
 </ul>
-  <!-- put pagination functions here -->
 	<?php wp_reset_postdata(); ?>
  
 <?php else : ?>
