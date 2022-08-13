428ecce6e3d4552913f2b4c83548e0c742a216bd
Datureli <soleris1@gmail.com>
2022-08-13 15:42:08 +0200

    set footer links

---
+++ about-page.php
@@ -0,0 +1,2 @@
+1
+<?php /* Template Name: About-page */ ?>
--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
 <footer class="footer">
+    <div style="background-color: #e9e8e8; height: 250px;">
+        <a href="https://developress.io/" target="_blank">DeveloPress</a>
+  
+<?php $policy_page = get_option( 'wp_page_for_privacy_policy' ); ?>
 
+<?php if($policy_page): ?>
+    <a href="<?php echo esc_url(get_permalink($policy_page)) ?>" target="_blank"><?php echo esc_html(get_the_title($policy_page)); ?></a>
+<?php endif; ?>
+</div>
 </footer>
 
 <?php
