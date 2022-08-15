5655efc0db3d9de67484b6f3a8b5a7f68ca3d156
Datureli <soleris1@gmail.com>
2022-08-15 19:20:20 +0200

    fix blog posts

---
+++ home.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() && is_home() ) : ?>
+ 
+ <ul style="list-style: none;">
+  
+     <?php 
+     while ( have_posts() ) :
+         the_post(); 
+         ?>
+         <li style="display: flex;">
+             
+         </style> 
+ 
+                <?php if ( has_post_thumbnail() ) : ?>
+                 <a href="<?php the_permalink(); ?>">
+                     <?php the_post_thumbnail( 'medium' ); ?>	
+                </a>		
+                <?php endif; ?>
+             <div style="margin-left: 20px">
+             <a href="<?php the_permalink(); ?>"><?php the_title( '<h2>', '</h2>' ); ?> </a>
+               <?php the_excerpt(); ?> 
+               <?php echo get_the_date( get_option( 'date_format' ) ); ?>
+             </div>
+          
+         </li>
+ 
+     <?php endwhile; ?>
+  
+ </ul>
+     <?php wp_reset_postdata(); ?>
+     <div style="justify-content: center; margin: auto; display: flex;">
+         <h2><?php the_posts_pagination(); ?></h2> 
+     </div>
+    
+ <?php else : ?>
+     <?php if ( is_home() ) : ?>
+     <p><?php _e( 'Sorry, no posts here' ); ?></p>
+     <?php endif; ?>
+ <?php endif; ?>
+ <?php get_footer(); ?>
